06dabf594cfbeb924b084e7fcd3657ef84f17664
thienphu <quyvo2079@gmail.com>
2025-11-04 08:02:00 +0700

    hieu

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,17 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
+};
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
+	cout<<"Hieu cua hai so "<<5<<" "<<6<<" la: "<<hieu(5,6);
 	system("pause");
 	return 0;
 }
